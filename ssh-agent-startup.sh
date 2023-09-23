93c969a38fe9fc00e9afa96fcce52c927e01f386
Bookermind <48366321+Bookermind@users.noreply.github.com>
2023-09-23 21:45:05 +0100

    Update ssh-agent-startup.sh

--- ssh-agent-startup.sh
+++ ssh-agent-startup.sh
@@ -2,6 +2,7 @@
 
 #Make the SSH_ASKPASS environment variable hold the location of ksshaskpass
 #This path is specific to OpenSuse Tumbleweed
+#Opensuse Leap 15.5 for example has /usr/lib/ssh/ksshaskpass
 #Will need altering for other Linux distro's
 #The more common location is /usr/bin/ksshaskpass
 
